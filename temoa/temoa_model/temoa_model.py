2066bee5412c432962874609360c5044e930f6a8
SutubraResearch <cameron@sutubra.ca>
2025-01-23 14:48:47 -0400

    Yet another temporary fix :"

--- temoa/temoa_model/temoa_model.py
+++ temoa/temoa_model/temoa_model.py
@@ -307,7 +307,7 @@ class TemoaModel(AbstractModel):
         M.TechOutputSplit = Param(M.regions, M.time_optimize, M.tech_all, M.commodity_carrier)
 
         M.RenewablePortfolioStandardConstraint_rpg = Set(
-            within=M.RegionalGlobalIndices * M.time_optimize * M.tech_group_names
+            within=M.RegionPlusGlobalIndices * M.time_optimize * M.tech_group_names
         )
         M.RenewablePortfolioStandard = Param(M.RenewablePortfolioStandardConstraint_rpg)
 
@@ -467,7 +467,7 @@ class TemoaModel(AbstractModel):
         M.CapacityCredit = Param(
             M.RegionalIndices, M.time_optimize, M.tech_all, M.vintage_all, default=0
         )
-        M.PlanningReserveMargin = Param(M.RegionalGlobalIndices, default=0.2)
+        M.PlanningReserveMargin = Param(M.RegionPlusGlobalIndices, default=0.2)
         # Storage duration is expressed in hours
         M.StorageDuration = Param(M.regions, M.tech_storage, default=4)
         # Initial storage charge level, expressed as fraction of full energy capacity.
