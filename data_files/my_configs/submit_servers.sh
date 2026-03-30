65139e649f4d67e77a871b530e19259dbc8d8ab4
Cameron Wade <cameron@sutubra.ca>
2026-03-30 15:07:00 -0300

    Use yes pipe instead of --silent to get verbose output

--- data_files/my_configs/submit_servers.sh
+++ data_files/my_configs/submit_servers.sh
@@ -12,4 +12,4 @@ source /trace/packages/anaconda3/2023.03-1/etc/profile.d/conda.sh
 conda activate temoa
 
 cd /trace/home/cwade2/code_repos/temoa_github/temoa
-temoa run --silent data_files/my_configs/config_servers.toml
+yes | temoa run data_files/my_configs/config_servers.toml
