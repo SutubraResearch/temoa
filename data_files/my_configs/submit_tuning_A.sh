b93d1b2b2266b34cd59e497d3c0d2d44c770cb08
Cameron Wade <cameron@sutubra.ca>
2026-04-07 10:22:49 -0300

    Add env-variable Gurobi tuning and experiment sbatch scripts
    Gurobi parameters (Method, Threads, BarOrder, ScaleFlag, PreSparsify,
    BarHomogeneous) can be overridden via TEMOA_* environment variables.
    Three experiment scripts for srv_20 with 64 threads: A (baseline),
    B (+PreSparsify +ScaleFlag), C (PDHG Method=6).

---
+++ data_files/my_configs/submit_tuning_A.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=tune_A
+#SBATCH --output=/trace/group/adams/cwade2/temoa_v4/servers/tune_A_%j.out
+#SBATCH --error=/trace/group/adams/cwade2/temoa_v4/servers/tune_A_%j.err
+#SBATCH --mem=300G
+#SBATCH --time=23:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH -p adams
+
+source /trace/packages/anaconda3/2023.03-1/etc/profile.d/conda.sh
+conda activate temoa
+
+cd /trace/home/cwade2/code_repos/temoa_github/temoa
+
+# Experiment A: baseline params + 64 threads
+export TEMOA_THREADS=16
+
+temoa run --silent data_files/my_configs/config_srv_20_tuneA.toml
---
+++ data_files/my_configs/submit_tuning_B.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=tune_B
+#SBATCH --output=/trace/group/adams/cwade2/temoa_v4/servers/tune_B_%j.out
+#SBATCH --error=/trace/group/adams/cwade2/temoa_v4/servers/tune_B_%j.err
+#SBATCH --mem=300G
+#SBATCH --time=23:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH -p adams
+
+source /trace/packages/anaconda3/2023.03-1/etc/profile.d/conda.sh
+conda activate temoa
+
+cd /trace/home/cwade2/code_repos/temoa_github/temoa
+
+# Experiment B: 64 threads + PreSparsify + aggressive scaling
+export TEMOA_THREADS=16
+export TEMOA_PRE_SPARSIFY=1
+export TEMOA_SCALE_FLAG=2
+
+temoa run --silent data_files/my_configs/config_srv_20.toml
